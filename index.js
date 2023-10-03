1fda46a7571ce166384c07a52eee3d3f5e57a20a
Natalie Burgei <natalieburgei04@gmail.com>
2023-10-03 11:46:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+let number = 1
+let random = number
+console.log(number)
+
+let num3 = 24
+let num4 = 5
+let mod = num3%num4
+console.log(mod)
+let max =20
+Math.max= (1,5,10,15,20)
+console.log (max)
